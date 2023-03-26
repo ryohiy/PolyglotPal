cd426d4768166e2213cc0edeeeadc119e7dc0caa
tanakenDX <ktanaka0807@gmail.com>
2023-03-26 22:16:30 +0900

    ver0.1.0

--- bot.py
+++ bot.py
@@ -10,22 +10,27 @@ polyglotpal_bot_key = api_keys["polyglotpal"]
 deepl_api_key = api_keys["deepl"]
 chatgpt_api_key = api_keys["chatgpt"]
 
-bot = commands.Bot(command_prefix='!')
-
-@bot.command()
-async def ask(ctx, *, user_input: str):
+# Intentsオブジェクトの作成
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+print('testaaaa')
+def ask(user_input):
     translated_input = translate_japanese_to_english(user_input, deepl_api_key)
-    print(user_input)
-    print(translated_input)
-    print(chatgpt_api_key)
     chatgpt_response = interact_with_chatgpt(translated_input, chatgpt_api_key)
-    print(translated_input)
-    print(chatgpt_response)
     translated_response = translate_english_to_japanese(chatgpt_response, deepl_api_key)
-    await ctx.send(translated_response)
+    return translated_response
+@client.event
+async def on_message(message):
+    # if message.author.bot:
+    #     print('エラー')
+    #     return
+    if message.content.startswith("!ask"):
+        print('何でも聞いて')
+        await message.channel.send("何でも聞いて")
+        wait_message = await client.wait_for("message")
 
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(f"エラーが発生しました: {str(error)}")
-
-bot.run(polyglotpal_bot_key)
+        print(wait_message.content)
+        res = ask(wait_message.content)
+        print(res)
+        await message.channel.send(res)
+client.run(polyglotpal_bot_key)
